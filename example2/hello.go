7fded20fd644867ed48ed33ec80374d5c7fd7aa0
Siddhardha Nandipati <siddhardhan@gmail.com>
2019-03-19 22:50:20 -0400

    adding jib example

--- example2/hello.go
+++ example2/hello.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	for {
 
-		fmt.Println(time.Now().Format(time.RFC850) + " Hello Skaffold - do you like the demo???")
+		fmt.Println(time.Now().Format(time.RFC850) + " Hello Skaffold - this is Sid :)")
 
 		time.Sleep(time.Second * 5)
 	}
